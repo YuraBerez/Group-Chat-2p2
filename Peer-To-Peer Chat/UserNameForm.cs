370857a4cbcb7d40905e39ec88ee56d253a2ded9
Yurii Bereza <1313adas1313@gmail.com>
2019-05-26 21:33:24 +0300

    Добавьте файлы проекта.

---
+++ Peer-To-Peer Chat/UserNameForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Peer_To_Peer_Chat
+{
+    public partial class UserNameForm : Form
+    {
+        public UserNameForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
