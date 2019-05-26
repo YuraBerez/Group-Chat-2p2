9be8be363cb69d83c972206e1aeca1e7b536af58
Yurii Bereza <1313adas1313@gmail.com>
2019-05-27 01:15:20 +0300

    Form creating user

--- Peer-To-Peer Chat/UserNameForm.Designer.cs
+++ Peer-To-Peer Chat/UserNameForm.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Create User name";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(15, 29);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(316, 22);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(337, 28);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "OK";
+            this.btnCreate.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // UserNameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(423, 63);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UserNameForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Creating user";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UserNameForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Button btnCreate;
     }
 }
 
--- Peer-To-Peer Chat/UserNameForm.cs
+++ Peer-To-Peer Chat/UserNameForm.cs
@@ -15,6 +15,40 @@ namespace Peer_To_Peer_Chat
         public UserNameForm()
         {
             InitializeComponent();
+            this.FormClosing += new FormClosingEventHandler(LoginForm_FormClosing);
+            btnCreate.Click += new EventHandler(btnCreate_Click);
+        }
+
+        string userName = "";
+
+        public string UserName
+        {
+            get { return userName; }
+        }
+
+
+        void LoginForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            userName = "";
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            userName = txtUserName.Text.Trim();
+
+            if (string.IsNullOrEmpty(userName))
+            {
+                MessageBox.Show("Please select a user name up to 32 characters.");
+                return;
+            }
+
+            this.FormClosing -= LoginForm_FormClosing;
+            this.Close();
+        }
+
+        private void UserNameForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            userName = "";
         }
     }
 }
